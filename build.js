c547925a61f14bf715f132fd82edd89cec834b27
Julius Rajala <juliusrajala@gmail.com>
2018-01-13 12:52:45 +0200

    Adjusted build scripts, folder structure, template.

--- build.js
+++ build.js
@@ -1,15 +1,19 @@
-var metalsmith = require('metalsmith');
-var markdown = require('metalsmith-markdown');
-var filter = require('metalsmith-filter');
-var layouts = require('metalsmith-layouts');
+const metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown');
+const filter = require('metalsmith-filter');
+const layouts = require('metalsmith-layouts');
+const handlebarHelpers = require('./lib/handlebarHelpers');
+
 
 metalsmith(__dirname)
-  .source('src')
-  .destination('dist')
+  .source('./src/content/')
+  .destination('dist/')
   .use(markdown())
+  .use(handlebarHelpers())
   .use(layouts({
     'engine':'handlebars', 
-    'directory':'src/layouts'
+    'directory':'src/layouts',
+    'partials': 'src/partials'
   }))
   .use(filter(['!*.scss', '!dist/*', '*.md', '*.html']))
   .build(function(err){
